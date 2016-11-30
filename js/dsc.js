24b3e7f1c75a70af91ca0420d19cb50e86c82483
Neal Kaviratna <Nkaviratna@gatech.edu>
2016-11-29 22:32:39 -0500

    Additional functions translated from CDC code

--- js/dsc.js
+++ js/dsc.js
@@ -20,6 +20,13 @@ var isMale = true;
 var _rrCondom, _rrArt, _rrPrepHetro, _rrPrepMsm, _rrCircVag, _rrCircAnal;
 _rrCondom = RR_CONDOM;
 
+var InsertVagProtectedRiskFactor, InsertVagUnprotectedRiskFactor,
+    ReceptiveVagProtectedRiskFactor, ReceptiveVagUnprotectedRiskFactor,
+    InsertAnalProtectedRiskFactor, InsertAnalUnprotectedRiskFactor,
+    ReceptiveAnalProtectedRiskFactor, ReceptiveAnalUnprotectedRiskFactor,
+    GiveOralProtectedRiskFactor, GiveOralUnprotectedRiskFactor,
+    ReceiveOralProtectedRiskFactor, ReceiveOralUnprotectedRiskFactor = 1;
+
 function UpdateArtRatios(isOnArt) {
     if (isOnArt)
         _rrArt = RR_ART;
@@ -62,7 +69,7 @@ function calcInsertVagProtectedRiskFactor(timesPerMonth, percentWithCondomUsage)
 
     var riskFactor = Math.pow(1-(pIV/(_rrCondom*_rrArt*_rrPrepHetro*_rrCircVag)), timesPerMonth*percentWithCondomUsage);
 
-    return riskFactor;
+    InsertVagProtectedRiskFactor = riskFactor;
 }
 
 function calcInsertVagUnprotectedRiskFactor(timesPerMonth, percentWithCondomUsage)
@@ -72,7 +79,7 @@ function calcInsertVagUnprotectedRiskFactor(timesPerMonth, percentWithCondomUsag
 
    var riskFactor = Math.pow(1-(pIV/(_rrArt*_rrPrepHetro*_rrCircVag)), actsPerMonth*(1-percentCondomUsage));
 
-   return riskFactor;
+   InsertVagUnprotectedRiskFactor = riskFactor;
 }
 
 
@@ -83,7 +90,7 @@ function calcReceptiveVagProtectedRiskFactor(timesPerMonth, percentWithCondomUsa
 
     var riskFactor = Math.pow(1-(pRV/(_rrCondom*_rrArt*_rrPrepHetro)), actsPerMonth*percentCondomUsage);
 
-    return riskFactor;
+    ReceptiveVagProtectedRiskFactor =  riskFactor;
 
 }
 function calcReceptiveVagUnprotectedRiskFactor(timesPerMonth, percentWithCondomUsage)
@@ -93,7 +100,7 @@ function calcReceptiveVagUnprotectedRiskFactor(timesPerMonth, percentWithCondomU
 
     var riskFactor = Math.pow(1-(pRV/(_rrArt*_rrPrepHetro)), actsPerMonth*(1-percentCondomUsage));
 
-    return riskFactor;
+    ReceptiveVagUnprotectedRiskFactor = riskFactor;
 
 }
 
@@ -104,7 +111,7 @@ function calcReceiveOralProtectedRiskFactor(timesPerMonth, percentWithCondomUsag
 
     var riskFactor = Math.pow(1-(pIO/(_rrCondom*_rrArt*_rrPrepHetro)), actsPerMonth*percentCondomUsage);
 
-    return riskFactor;
+    ReceiveOralProtectedRiskFactor = riskFactor;
 
 }
 
@@ -115,7 +122,7 @@ function calcReceiveOralUnprotectedRiskFactor(timesPerMonth, percentWithCondomUs
 
     var riskFactor =Math.pow(1-(pIO/(_rrArt*_rrPrepHetro)), actsPerMonth*(1-percentCondomUsage));
 
-    return riskFactor;
+    ReceiveOralUnprotectedRiskFactor = riskFactor;
 
 }
 
@@ -126,7 +133,7 @@ function calcGiveOralProtectedRiskFactor(timesPerMonth, percentWithCondomUsage)
 
     var riskFactor =Math.pow(1-(pRO/(_rrCondom*_rrArt*_rrPrepHetro)), actsPerMonth*percentCondomUsage);
 
-    return riskFactor;
+    GiveOralProtectedRiskFactor = riskFactor;
 
 }
 
@@ -137,7 +144,7 @@ function calcGiveOralUnprotectedRiskFactor(timesPerMonth, percentWithCondomUsage
 
     var riskFactor =Math.pow(1-(pRO/(_rrArt*_rrPrepHetro)), actsPerMonth*(1-percentCondomUsage));
 
-    return riskFactor;
+    GiveOralUnprotectedRiskFactor = riskFactor;
 
 }
 
@@ -148,7 +155,7 @@ function calcInsertAnalProtectedRiskFactor(timesPerMonth, percentWithCondomUsage
 
     var riskFactor =Math.pow(1-(pIA/(_rrCondom*_rrArt*_rrPrepMsm*_rrCircAnal)), actsPerMonth*percentCondomUsage);
 
-    return riskFactor;
+    InsertAnalProtectedRiskFactor = riskFactor;
 
 
 }
@@ -160,7 +167,7 @@ function calcInsertAnalUnprotectedRiskFactor(timesPerMonth, percentWithCondomUsa
 
     var riskFactor =Math.pow(1-(pIA/(_rrArt*_rrPrepMsm*_rrCircAnal)), actsPerMonth*(1-percentCondomUsage));
 
-    return riskFactor;
+    InsertAnalUnprotectedRiskFactor = riskFactor;
 
 
 }
@@ -172,8 +179,8 @@ function calcReceptiveAnalProtectedRiskFactor(timesPerMonth, percentWithCondomUs
 
     var riskFactor =Math.pow(1-(pRA/(_rrCondom*_rrArt*_rrPrepMsm)), actsPerMonth*percentCondomUsage);
 
+    ReceptiveAnalProtectedRiskFactor = riskFactor;
     return riskFactor;
-
 
 }
 
@@ -184,7 +191,77 @@ function calcReceptiveAnalUnprotectedRiskFactor(timesPerMonth, percentWithCondom
 
     var riskFactor =Math.pow(1-(pRA/(_rrArt*_rrPrepMsm)), actsPerMonth*(1-percentCondomUsage));
 
-    return riskFactor;
+    ReceptiveAnalUnprotectedRiskFactor = riskFactor;
+}
 
 
+function updateStats ()
+{
+    var _chancesPerMonthPercent = 1 - (InsertVagUnprotectedRiskFactor * InsertVagProtectedRiskFactor *
+                          ReceptiveVagProtectedRiskFactor * ReceptiveVagUnprotectedRiskFactor *
+                          ReceiveOralProtectedRiskFactor * ReceiveOralUnprotectedRiskFactor *
+                          GiveOralProtectedRiskFactor * GiveOralUnprotectedRiskFactor *
+                          InsertAnalProtectedRiskFactor * InsertAnalUnprotectedRiskFactor *
+                          ReceptiveAnalProtectedRiskFactor * ReceptiveAnalUnprotectedRiskFactor);
+
+    var _chancesPerMonthRatio = 1 / _chancesPerMonthPercent;
+
+    var _chancesPerYearPercent = 1-Math.pow(1-_chancesPerMonthPercent,12);
+    var _chancesPerYearRatio = 1 / _chancesPerYearPercent;
+
+    var _chancesPerTenYearPercent = 1-Math.pow(1-_chancesPerYearPercent,10);
+    var _chancesPerTenYearRatio = 1 / _chancesPerTenYearPercent;
+
+    var _chancesPerTwentyFiveYearPercent = 1-Math.pow(1-_chancesPerYearPercent,25);
+    var _chancesPerTwentyFiveYearRatio = 1 / _chancesPerTwentyFiveYearPercent;
+
+
+    var _riskProductByIV = InsertVagUnprotectedRiskFactor * InsertVagProtectedRiskFactor;
+    var _riskProductByRV = ReceptiveVagProtectedRiskFactor * ReceptiveVagUnprotectedRiskFactor;
+    var _riskProductByRO = ReceiveOralProtectedRiskFactor * ReceiveOralUnprotectedRiskFactor;
+    var _riskProductByGO = calcGiveOralProtectedRiskFactor * GiveOralUnprotectedRiskFactor;
+    var _riskProductByIA = calcInsertAnalProtectedRiskFactor * InsertAnalUnprotectedRiskFactor;
+    var _riskProductByRA = calcReceptiveAnalProtectedRiskFactor * ReceptiveAnalUnprotectedRiskFactor;
+
+    var _totalProtectedRiskFactor = InsertVagProtectedRiskFactor * ReceptiveVagProtectedRiskFactor * ReceiveOralProtectedRiskFactor *
+      GiveOralProtectedRiskFactor * InsertAnalProtectedRiskFactor * ReceptiveAnalProtectedRiskFactor;
+
+    var _totalUnprotectedRiskFactor = _totalProtectedRiskFactor * InsertVagUnprotectedRiskFactor
+    * ReceptiveVagUnprotectedRiskFactor * ReceiveOralUnprotectedRiskFactor
+    * GiveOralUnprotectedRiskFactor * InsertAnalUnprotectedRiskFactor
+    * ReceptiveAnalUnprotectedRiskFactor;
+
+    var _riskByIV = 1 - _riskProductByIV;
+    var _riskByRV = 1 - _riskProductByRV;
+    var _riskByRO = 1 - _riskProductByRO;
+    var _riskByGO = 1 - _riskProductByGO;
+    var _riskByIA = 1 - _riskProductByIA;
+    var _riskByRA = 1 - _riskProductByRA;
+
+
+    var totalContribToRisk = _riskByIV + _riskByRV + _riskByRO + _riskByGO + _riskByIA + _riskByRA;
+    var _ivContribPercent = (_riskByIV / totalContribToRisk) * 100;
+    var _rvContribPercent = (_riskByRV / totalContribToRisk) * 100;
+    var _roContribPercent = (_riskByRO / totalContribToRisk) * 100;
+    var _goContribPercent = (_riskByGO / totalContribToRisk) * 100;
+    var _raContribPercent = (_riskByRA / totalContribToRisk) * 100;
+    var _iaContribPercent = (_riskByIA / totalContribToRisk) * 100;
+
+    var _ivPieSlice = (_riskByIV / totalContribToRisk) * 360;
+    var _rvPieSlice = (_riskByRV / totalContribToRisk) * 360;
+    var _roPieSlice = (_riskByRO / totalContribToRisk) * 360;
+    var _goPieSlice = (_riskByGO / totalContribToRisk) * 360;
+    var _raPieSlice = (_riskByRA / totalContribToRisk) * 360;
+    var _iaPieSlice = (_riskByIA / totalContribToRisk) * 360;
+
+
+    var riskUnprotected = 1 - _totalUnprotectedRiskFactor;
+    var riskProtected = 1- _totalProtectedRiskFactor;
+
+    var totalCondomUsageContributions = riskProtected + riskUnprotected;
+
+    var _protectedPieSlice = (riskProtected/ totalCondomUsageContributions) * 360;
+    var _unprotectedPieSlice = (riskUnprotected/ totalCondomUsageContributions) * 360;
+    var _protectedPercent = (riskProtected/ totalCondomUsageContributions) * 100;
+    var _unprotectedPercent = (riskUnprotected/ totalCondomUsageContributions) * 100;
 }
